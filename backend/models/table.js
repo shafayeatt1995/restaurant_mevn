f1315455e2fbb9e81bc197280c9e34f3c16a2f4f
shafayeatt1995 <shafayetalanik@gmail.com>
2024-01-10 00:46:07 +0600

    Table updated

--- backend/models/table.js
+++ backend/models/table.js
@@ -1,44 +1,15 @@
 const mongoose = require("mongoose");
-const { randomKey } = require("@/backend/utils");
 const Schema = mongoose.Schema;
 
 const TableSchema = new Schema(
   {
     restaurantID: { type: String, required: true, index: true },
     name: { type: String, required: true },
-    serial: { type: Number, unique: true },
   },
   {
     strict: true,
     timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
   }
 );
-
-// TableSchema.pre("save", async function (next) {
-//   const doc = this;
-//   if (!doc.isNew) {
-//     return next();
-//   }
-
-//   try {
-//     const lastTable = await doc.constructor
-//       .findOne({}, {}, { sort: { _id: -1 } })
-//       .limit(1);
-//     if (lastTable) {
-//       doc.serial = +lastTable.serial.slice(0, -2) + 1 + randomKey(2, true);
-//     } else {
-//       doc.serial = 1 + randomKey(2, true);
-//     }
-
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-TableSchema.plugin(plugin, {
-  model: "Table",
-  field: "serial",
-  startAt: 1,
-});
 
 module.exports = mongoose.model("Table", TableSchema);
