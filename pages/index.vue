c462a8ae1e0da098d4b56202d1df885d2846a19d
shafayeatt1995 <shafayetalanik@gmail.com>
2024-02-05 11:37:53 +0600

    Dashboard little updated

--- pages/index.vue
+++ pages/index.vue
@@ -33,7 +33,7 @@
                 ? 'translate-x-0 opacity-100 '
                 : 'opacity-0 -translate-x-full',
             ]"
-            class="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white md:bg-transparent md: md:mt-0 md:p-0 md:top-0 md:relative md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center"
+            class="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white md:bg-transparent md: md:mt-0 md:p-0 md:top-0 md:relative md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center shadow-xl"
           >
             <div class="flex flex-col md:flex-row md:mx-6">
               <a
@@ -69,63 +69,68 @@
           </div>
         </div>
       </nav>
-
       <div
-        class="container px-4 py-8 lg:py-16 mx-auto"
+        class="relative overflow-hidden"
         :style="isMobile ? `` : `height: calc(100vh - 60px)`"
       >
-        <div class="items-center flex h-full flex-col lg:flex-row">
-          <div class="flex-1">
-            <div class="">
-              <img src="/logo.png" class="h-20" />
-              <h1
-                class="text-3xl font-bold text-gray-800 lg:text-5xl uppercase mt-2"
-              >
-                Scan<span class="text-green-500">Eating</span>
-              </h1>
-              <h3 class="text-gray-700 text-xl lg:text-3xl">
-                A Complete Digital Menu Solution
-              </h3>
-              <p class="mt-3 text-gray-600 text-base lg:text-xl">
-                Take your restaurant one step further! It's time to dive into
-                the digital world. Give your customers a modern experience with
-                a menu they can access on their smartphones using a simple QR
-                code. No more paper menus – just scan, browse, and order
-                <span class="text-2xl">😊</span>
-              </p>
+        <div class="relative container px-4 py-8 lg:py-16 mx-auto h-full z-[2]">
+          <div class="items-center flex h-full flex-col lg:flex-row">
+            <div class="flex-1 flex flex-col justify-center">
+              <div class="">
+                <img src="/logo.png" class="h-20" />
+                <h1
+                  class="text-3xl font-bold text-gray-800 lg:text-5xl uppercase mt-2"
+                >
+                  Scan<span class="text-green-500">Eating</span>
+                </h1>
+                <h3 class="text-gray-700 text-xl lg:text-3xl">
+                  A Complete Digital Menu Solution
+                </h3>
+                <p class="mt-3 text-gray-600 text-base lg:text-xl">
+                  Take your restaurant one step further! It's time to dive into
+                  the digital world. Give your customers a modern experience
+                  with a menu they can access on their smartphones using a
+                  simple QR code. No more paper menus – just scan, browse, and
+                  order
+                  <span class="text-2xl">😊</span>
+                </p>
+              </div>
+              <div class="flex mt-5 gap-3">
+                <NuxtLink
+                  class="px-7 py-[10px] text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-green-500 rounded-lg lg:w-auto hover:bg-green-600 focus:outline-none focus:bg-green-500 font-bold"
+                  :to="{
+                    name: 'menu-slug-table',
+                    params: { slug: '1c', table: 'table-1-lx' },
+                  }"
+                  target="_blank"
+                >
+                  Demo Menu
+                </NuxtLink>
+                <NuxtLink
+                  class="px-7 py-[10px] text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-green-500 rounded-lg lg:w-auto hover:bg-green-600 focus:outline-none focus:bg-green-500 font-bold"
+                  :to="{
+                    name: 'auth-login',
+                    query: { demo: true },
+                  }"
+                >
+                  Demo Waiter
+                </NuxtLink>
+              </div>
             </div>
-            <div class="flex mt-5 gap-3">
-              <NuxtLink
-                class="px-7 py-[10px] text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-green-500 rounded-lg lg:w-auto hover:bg-green-600 focus:outline-none focus:bg-green-500 font-bold"
-                :to="{
-                  name: 'menu-slug-table',
-                  params: { slug: '1c', table: 'table-1-lx' },
-                }"
-                target="_blank"
-              >
-                Demo Menu
-              </NuxtLink>
-              <NuxtLink
-                class="px-7 py-[10px] text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-green-500 rounded-lg lg:w-auto hover:bg-green-600 focus:outline-none focus:bg-green-500 font-bold"
-                :to="{
-                  name: 'auth-login',
-                  query: { demo: true },
-                }"
-              >
-                Demo Waiter
-              </NuxtLink>
-            </div>
-          </div>
 
-          <div
-            class="hidden lg:flex items-center justify-center lg:flex-1 lg:mt-0 lg:w-1/2 lg:h-full"
-          >
-            <img
-              class="max-w-full h-96 lg:h-full object-contain"
-              src="/images/menu.webp"
-              alt="menu image"
-            />
+            <div
+              class="hidden lg:flex items-center justify-center lg:flex-1 lg:mt-0 lg:w-1/2 lg:h-full"
+            >
+              <img
+                class="max-w-full h-96 lg:h-[70vh] object-contain"
+                src="/images/menu.webp"
+                alt="menu image"
+              />
+            </div>
           </div>
+        </div>
+        <div class="absolute bottom-0 left-0 right-0 z-[1]" v-if="!isMobile">
+          <Wave />
         </div>
       </div>
     </header>
@@ -186,9 +191,11 @@
 
 <script>
 import { mapGetters } from "vuex";
+import Wave from "@/static/images/wave.svg";
 export default {
   name: "indexPage",
   auth: false,
+  components: { Wave },
   data() {
     return {
       isOpen: false,
