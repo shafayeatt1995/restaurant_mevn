1e0be142f6673bee91ebb6cf2dc0e587fbe847ba
shafayeatt1995 <shafayetalanik@gmail.com>
2024-02-08 14:23:15 +0600

    Initialy updated

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { isAuthenticated } = require("@/backend/middleware");
+const { printer } = require("node-thermal-printer");
 
 router.use("/auth", require("./auth"));
 router.use("/menu", require("./menu"));
@@ -13,5 +14,27 @@ router.use("/manager", require("./manager"));
 router.use("/user", require("./user"));
 router.use("/item", require("./item"));
 router.use("/mow", require("./mow"));
+
+const selectedPrinter = "DIR-E58";
+router.get("/print", async (req, res) => {
+  try {
+    printer.init({
+      type: "star",
+      interface: selectedPrinter,
+      characterSet: "SLOVENIA",
+    });
+
+    printer.println("Your text to print");
+
+    await printer.execute();
+    res.status(200).send("Server ok");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  } finally {
+    // Close the printer
+    printer.close();
+  }
+});
 
 module.exports = router;
