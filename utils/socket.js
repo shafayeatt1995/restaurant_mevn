13ec743edea37628a9dc64290483a8bcfc4ac250
shafayeatt1995 <shafayetalanik@gmail.com>
2024-02-16 15:28:08 +0600

    socket updated

--- utils/socket.js
+++ utils/socket.js
@@ -1,11 +1,3 @@
 import io from "socket.io-client";
-
 const socket = io(process.env.BASE_URL);
-socket.on("connect", () => {
-  console.log("Connected to Socket.IO server");
-});
-socket.on("connect_error", (error) => {
-  console.error("Failed to connect to Socket.IO server:", error);
-});
-
 export default socket;
