092b8b0da317cfce4b4a936a417cb5437fdf37d0
shafayeatt1995 <shafayetalanik@gmail.com>
2024-02-21 00:26:22 +0600

    Subscription updated

--- components/settings/Subscription.vue
+++ components/settings/Subscription.vue
@@ -18,47 +18,58 @@
         <div
           class="w-full p-8 space-y-8 text-center border border-gray-200 rounded-lg"
         >
-          <p class="font-medium text-gray-700 uppercase">1 Month</p>
+          <div class="flex flex-col justify-between h-full">
+            <div>
+              <p class="font-medium text-gray-700 uppercase">1 Month</p>
 
-          <h2 class="text-4xl font-semibold text-gray-800 uppercase">৳500</h2>
-
-          <button
-            class="w-full px-4 py-2 mt-10 tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-900 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80"
-          >
-            Active
-          </button>
+              <h2 class="text-4xl font-semibold text-gray-800 uppercase mt-8">
+                ৳500
+              </h2>
+            </div>
+            <button
+              class="w-full px-4 py-2 mt-10 tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-900 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80"
+            >
+              Active
+            </button>
+          </div>
         </div>
 
         <div class="w-full p-8 space-y-8 text-center bg-gray-900 rounded-lg">
-          <p class="font-medium text-gray-200 uppercase">6 Months</p>
+          <div class="flex flex-col justify-between h-full">
+            <div>
+              <p class="font-medium text-gray-200 uppercase">6 Months</p>
 
-          <h2 class="text-5xl font-bold text-white uppercase">
-            <del class="text-2xl">৳3000</del>
-            <p class="">৳2500</p>
-          </h2>
-
-          <button
-            class="w-full px-4 py-2 mt-10 tracking-wide text-gray-900 capitalize transition-colors duration-300 transform bg-white rounded-md hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:ring focus:ring-gray-200 focus:ring-opacity-80 font-bold"
-          >
-            Active
-          </button>
+              <h2 class="text-5xl font-bold text-white uppercase">
+                <del class="text-2xl">৳3000</del>
+                <p class="">৳2500</p>
+              </h2>
+            </div>
+            <button
+              class="w-full px-4 py-2 mt-10 tracking-wide text-gray-900 capitalize transition-colors duration-300 transform bg-white rounded-md hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:ring focus:ring-gray-200 focus:ring-opacity-80 font-bold"
+            >
+              Active
+            </button>
+          </div>
         </div>
 
         <div
           class="w-full p-8 space-y-8 text-center border border-gray-200 rounded-lg"
         >
-          <p class="font-medium text-gray-500 uppercase">12 Months</p>
+          <div class="flex flex-col justify-between h-full">
+            <div>
+              <p class="font-medium text-gray-500 uppercase">12 Months</p>
 
-          <h2 class="text-4xl font-semibold text-gray-800 uppercase">
-            <del class="text-2xl">৳6000</del>
-            <p class="">৳5000</p>
-          </h2>
-
-          <button
-            class="w-full px-4 py-2 mt-10 tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-900 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80"
-          >
-            Active
-          </button>
+              <h2 class="text-4xl font-semibold text-gray-800 uppercase">
+                <del class="text-2xl">৳6000</del>
+                <p class="">৳5000</p>
+              </h2>
+            </div>
+            <button
+              class="w-full px-4 py-2 mt-10 tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-900 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80"
+            >
+              Active
+            </button>
+          </div>
         </div>
       </div>
     </div>
