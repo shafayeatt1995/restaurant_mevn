7fd26f10a8287404126fdd3432ba352c280faa03
shafayeatt1995 <shafayetalanik@gmail.com>
2024-02-24 20:04:57 +0600

    Google tag manager updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,16 +39,7 @@ export default {
     ],
     script: [
       {
-        hid: "google-tag-manager-script",
-        innerHTML: `
-          <!-- Google Tag Manager -->
-          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NKNCBRGH');</script>
-          <!-- End Google Tag Manager -->
-        `,
+        src: `/gtag-head.js`,
         head: true,
       },
       {
@@ -59,18 +50,6 @@ export default {
       {
         type: "text/javascript",
         src: `/gtag.js`,
-        body: true,
-      },
-    ],
-    noscript: [
-      {
-        hid: "google-tag-manager-noscript",
-        innerHTML: `
-          <!-- Google Tag Manager (noscript) -->
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKNCBRGH"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-          <!-- End Google Tag Manager (noscript) -->
-        `,
         body: true,
       },
     ],
--- plugins/google-fonts.js
+++ plugins/google-fonts.js
@@ -11,6 +11,18 @@ export default function () {
     fa.href =
       "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css";
     document.head.appendChild(fa);
+
+    const noscriptIframe = document.createElement("noscript");
+    const iframe = document.createElement("iframe");
+    iframe.setAttribute(
+      "src",
+      "https://www.googletagmanager.com/ns.html?id=GTM-NKNCBRGH"
+    );
+    iframe.setAttribute("height", "0");
+    iframe.setAttribute("width", "0");
+    iframe.setAttribute("style", "display:none;visibility:hidden");
+    noscriptIframe.appendChild(iframe);
+    document.body.appendChild(noscriptIframe);
   }
 
   window.addEventListener("load", loadScript);
