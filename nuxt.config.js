9f0313c73cef00d7c810a2eaf2fd739619c0d4e8
shafayeatt1995 <shafayetalanik@gmail.com>
2024-03-03 15:47:42 +0600

    Nuxt.config.js is updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
       {
         hid: "og:image",
         property: "og:image",
-        content: "/og-image.webp",
+        content: "https://scaneating.com/og-image.webp",
       },
     ],
     link: [
