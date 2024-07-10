e2925db4a12b413dcabd15bb4713ac07bc3e07a3
shafayeatt1995 <shafayetalanik@gmail.com>
2024-07-11 03:02:12 +0600

    Token cut updated

--- backend/controllers/MenuController.js
+++ backend/controllers/MenuController.js
@@ -17,46 +17,52 @@ const controller = {
         logo: 1,
         name: 1,
         authOrder: 1,
+        orderToken: 1,
+        tokenPerOrder: 1,
       });
-      let findTable;
-      let tableList;
-      if (table === undefined) {
-        tableList = await Table.find({
+      if (restaurant.orderToken >= restaurant.tokenPerOrder) {
+        let findTable;
+        let tableList;
+        if (table === undefined) {
+          tableList = await Table.find({
+            restaurantID: restaurant._id,
+          });
+        } else {
+          const tableFindOne = await Table.findOne({
+            restaurantID: restaurant._id,
+            serial: table ?? "",
+          });
+          if (tableFindOne) {
+            findTable = tableFindOne;
+          } else {
+            throw new Error("Couldn't find");
+          }
+        }
+        const categories = await Category.find({
           restaurantID: restaurant._id,
+        }).sort({ serial: 1 });
+        const featureCategories = await FeatureCategory.find({
+          restaurantID: restaurant._id,
+        }).sort({ serial: 1 });
+        const subCategories = await SubCategory.find({
+          restaurantID: restaurant._id,
+        }).sort({ serial: 1 });
+        const items = await Item.find({
+          restaurantID: restaurant._id,
+        }).sort({ serial: -1 });
+
+        res.status(200).json({
+          restaurant,
+          categories,
+          featureCategories,
+          subCategories,
+          items,
+          table: findTable || {},
+          tableList: tableList || [],
         });
       } else {
-        const tableFindOne = await Table.findOne({
-          restaurantID: restaurant._id,
-          serial: table ?? "",
-        });
-        if (tableFindOne) {
-          findTable = tableFindOne;
-        } else {
-          throw new Error("Couldn't find");
-        }
+        throw new error("purchase new order token");
       }
-      const categories = await Category.find({
-        restaurantID: restaurant._id,
-      }).sort({ serial: 1 });
-      const featureCategories = await FeatureCategory.find({
-        restaurantID: restaurant._id,
-      }).sort({ serial: 1 });
-      const subCategories = await SubCategory.find({
-        restaurantID: restaurant._id,
-      }).sort({ serial: 1 });
-      const items = await Item.find({
-        restaurantID: restaurant._id,
-      }).sort({ serial: -1 });
-
-      res.status(200).json({
-        restaurant,
-        categories,
-        featureCategories,
-        subCategories,
-        items,
-        table: findTable || {},
-        tableList: tableList || [],
-      });
     } catch (error) {
       res
         .status(500)
